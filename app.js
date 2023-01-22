c4cc9d84bea48c68bccb90038ba53c2a953bd250
abhi <abhi@windowspc.com>
2023-01-23 04:33:39 +0530

    making env work in development

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-require('dotenv').config(); 
 const path = require("path");
-
+require('dotenv').config(); 
 
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -14,10 +13,16 @@ const multer = require("multer");
 const errorController = require("./controllers/error");
 const User = require("./models/user");
 
-const MONGODB_URI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@section20shop.c57necz.mongodb.net/shop?retryWrites=true&w=majority`;
+const userName = process.env.MONGO_USER;
+const dbPass = process.env.MONGO_PASSWORD;
+
+
+
+const MONGODB_URI = `mongodb+srv://${userName}:${dbPass}@section20shop.c57necz.mongodb.net/shop?retryWrites=true&w=majority`;
 
 const app = express();
 
+console.log(MONGODB_URI);
 const store = new MongoDBStore({
   uri: MONGODB_URI,
   collection: "sessions",
